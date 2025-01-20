7ce18017463fe73910082c2e3a3331cf069b035d
Hiba Dawood <164556977+HibaDawood@users.noreply.github.com>
2025-01-20 18:51:16 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import HeroSection from "../components/Hero-section";
 import BestsellerProducts from "@/components/BestSelling-products";
 import Hero1 from "@/components/Hero1";
 import Hero2 from "@/components/Hero2";
-import Hero3 from "@/components/Hero3";
 
 export default function Home() {
   return (
@@ -14,7 +13,6 @@ export default function Home() {
     <BestsellerProducts/>
     <Hero1/>
     <Hero2 />
-    <Hero3/>
   
    </div>
   );
